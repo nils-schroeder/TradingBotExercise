422f4311226362a03d13c9ba3f0da6f0f53ae81c
Nils <nschdr@gmail.com>
2023-08-09 20:08:33 +0200

    javadoc strings

--- src/main/java/engine/ConstantStrategy.java
+++ src/main/java/engine/ConstantStrategy.java
@@ -2,12 +2,24 @@ package engine;
 
 import trading.BotState;
 
+/**
+ * Implements a strategy that consistently places a bid of a constant size.
+ * This strategy does not adapt to changes in the environment or the actions of the opposing player.
+ * It always bids the pre-determined size, irrespective of available cash or other factors.
+ */
 public class ConstantStrategy extends Strategy{
 
+    /**
+     * Determines the bid amount to be the pre-determined bid size.
+     * This strategy consistently returns the same bid amount.
+     *
+     * @param playerState The state of the bot/player (not used in this strategy).
+     * @param otherState The state of the opposing bot/player (not used in this strategy).
+     * @return The constant bid amount.
+     */
     @Override
     public int determineBid(BotState playerState, BotState otherState) {
 
         return bidSize;
-
     }
 }
--- src/main/java/engine/InverseGreedyStrategy.java
+++ src/main/java/engine/InverseGreedyStrategy.java
@@ -2,37 +2,50 @@ package engine;
 
 import trading.BotState;
 
+/**
+ * Implements the Inverse Greedy Strategy for determining bids.
+ * This strategy uses the principle of bidding aggressively during the latter half of the game,
+ * while placing no bids during the early phases.
+ */
 public class InverseGreedyStrategy extends Strategy{
 
+    /**
+     * Override of calculateBidSize to better suit the strategy.
+     * Here, the bid size is inversely proportional to the number of rounds left.
+     *
+     * @param playerState The state of the bot/player.
+     * @return The calculated bid size.
+     */
     @Override
     protected int calculateBidSize(BotState playerState){
-
         return playerState.getCash() / ((playerState.getAvailableQuantity() / 4) + 1);
-
     }
 
+    /**
+     * Determines if the current round is within the aggressive bid window.
+     *
+     * @param playerState The state of the bot/player.
+     * @return True if within the bid window, False otherwise.
+     */
     private boolean bidWindow(BotState playerState) {
-
         int numberOfRounds = playerState.getStartQuantity() / 2;
-
         int roundsLeft = playerState.getAvailableQuantity() / 2;
-
         return roundsLeft <= (numberOfRounds / 2) + 1;
-
     }
 
+    /**
+     * Determines the bid based on the current state of the game and the strategy.
+     *
+     * @param playerState The state of the bot/player.
+     * @param otherState The state of the opposing bot/player.
+     * @return The determined bid amount.
+     */
     @Override
     public int determineBid(BotState playerState, BotState otherState) {
-
         if (bidWindow(playerState)) {
-
             return playerState.getCash() >= bidSize ? bidSize : 0;
-
         } else {
-
             return 0;
-
         }
-
     }
 }
